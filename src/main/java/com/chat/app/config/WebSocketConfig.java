8acc54e8ffef6dcc5bce0f4ac76d845446d675aa
Bhavesh Rathore <111549277+bhaveshzx@users.noreply.github.com>
2025-08-30 02:51:40 +0530

    Update WebSocketConfig.java

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("https://real-time-chat-application-vju7.onrender.com/")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
